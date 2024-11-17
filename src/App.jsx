8c241895e75215f6588e4fbb72121098970a2567
ayabarakat3 <ayabarakat14@gmail.com>
2024-11-17 11:47:27 -0500

    Create ProductList component to render each ProductItem

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import ProductList from './ProductList';
+import AddProductForm from './AddProductForm';
+import './App.css';
+// Code created to import necessary information from other files. 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([
+    { id: 1, name: 'Mascara', price: 25.0, description: 'Lash lengthening, long-wearing, cruelty-free mascara.' },
+    { id: 2, name: 'Concealer', price: 28.0, description: 'A full coverage, light and liquid formula, best for brightening. ' },
+    { id: 3, name: 'Perfume', price: 87.0, description: 'Unisex, layerable, floral scent.' },
+  ]);
+ // Data created. Dataset includes id number, name, price, and description of products.
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const addProduct = (newProduct) => {
+    setProducts([...products, newProduct]);
+  };
+ // Code created to add new peoducts when needed. 
+
+  return ( 
+    <div>
+      <h1>Product Dashboard</h1>
+      <ProductList products={products} />
+      <AddProductForm onAddProduct={addProduct} />
+    </div>
+  );
 }
-
-export default App
+// Code created to show the product list and the product form to add new products. 
+export default App;
+// Code created to make App function accessible in other files.
---
+++ src/ProductList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'; 
+import ProductItem from './ProductItem'; 
+// Code created to import necessary information from other files.
+
+function ProductList({ products }) {
+  return (
+    <div>
+      <h2>Product List:</h2>
+      {products.map((product) => (
+        <ProductItem key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
+// Code created to display the list of products by the ID number that was set in the original data set. 
+
+export default ProductList;
+// Code created to make App function accessible in other files.
